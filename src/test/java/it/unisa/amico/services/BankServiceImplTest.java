560d50fcac7e068f6f30a32f827e8f086fd47fa0
marco <mark.98@live.it>
2019-12-16 16:17:40 +0100

    prova

--- src/test/java/it/unisa/amico/services/BankServiceImplTest.java
+++ src/test/java/it/unisa/amico/services/BankServiceImplTest.java
@@ -8,11 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
