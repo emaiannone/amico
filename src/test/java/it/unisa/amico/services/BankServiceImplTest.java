42802f67430d651f56fd12af5dc7f64685fab726
emaiannone <emaiannone96@gmail.com>
2019-12-16 13:25:23 +0100

    Fix Unit test

--- src/test/java/it/unisa/amico/services/BankServiceImplTest.java
+++ src/test/java/it/unisa/amico/services/BankServiceImplTest.java
@@ -11,8 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -39,8 +38,8 @@ class BankServiceImplTest {
     void getAllBankAccounts() {
         when(bankAccountDao.findAll()).thenReturn(new ArrayList<>());
         List<BankAccount> accounts1 = bankService.getAllBankAccounts();
-        // assertFalse(accounts1.isEmpty());
-        assertTrue(accounts1.isEmpty());
+        assertFalse(accounts1.isEmpty());
+        // assertTrue(accounts1.isEmpty());
     }
 
     @Test
